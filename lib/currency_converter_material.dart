64022159c5467457bb8c87ee31b59f56cb8e218c
Dipesh Budha Chhetri <154000610+dipesh738@users.noreply.github.com>
2025-12-16 17:45:24 +0545

    second commit

---
+++ lib/currency_converter_material.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterial extends StatelessWidget {
+  const CurrencyConverterMaterial({super.key});
+  @override
+  Widget build(BuildContext context) {
+    double result = 0;
+    final texteditingcontroller = TextEditingController();
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 2.0,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+      borderRadius: BorderRadius.circular(10),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text(
+          "Currency Converter",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              result.toString(),
+              style: TextStyle(
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: texteditingcontroller,
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: "Enter amount in USD",
+                  hintStyle: TextStyle(color: Colors.black),
+                  prefixIcon: Icon(Icons.attach_money_outlined),
+                  prefixIconColor: Colors.green,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  result = double.parse(texteditingcontroller.text) * 145;
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 20,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text("Convert"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
